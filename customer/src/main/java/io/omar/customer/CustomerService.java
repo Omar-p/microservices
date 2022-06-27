d73fca70ab83f51ba84c2361f75230e1722f2405
Omar Shabaan <omar.plus.077@gmail.com>
2022-06-27 23:22:14 +0200

    [customer] customer-service before register new customer make a check fraud by calling fraud-service

--- customer/src/main/java/io/omar/customer/CustomerService.java
+++ customer/src/main/java/io/omar/customer/CustomerService.java
@@ -2,10 +2,11 @@ package io.omar.customer;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public record CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate) {
 
   public void registerCustomer(CustomerRegistrationRequest request) {
     Customer customer = Customer.builder()
@@ -14,6 +15,19 @@ public record CustomerService(CustomerRepository customerRepository) {
         .email(request.email())
         .build();
 
-    customerRepository.save(customer);
+    // TODO: check if email valid
+    customerRepository.saveAndFlush(customer);
+    // TODO: check if email not taken
+    // TODO: check if fraudster
+    final FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+        "http://localhost:8081/api/v1/fraud-check/{customerId}",
+        FraudCheckResponse.class,
+        customer.getId()
+    );
+
+    if (fraudCheckResponse.isFraudster()) {
+      throw new IllegalStateException("fraudster");
+    }
+    // TODO: send notification
   }
 }
---
+++ customer/src/main/java/io/omar/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package io.omar.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
