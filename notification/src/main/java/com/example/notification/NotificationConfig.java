d79b76c281230fca1c90b7a3e95b8d62086bc498
Omar Shabaan <omar.plus.077@gmail.com>
2023-03-15 03:02:07 +0200

    fix property name

--- notification/src/main/java/com/example/notification/NotificationConfig.java
+++ notification/src/main/java/com/example/notification/NotificationConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class NotificationConfig {
-  @Value("${rabbitmq.exchanges.internal")
+  @Value("${rabbitmq.exchanges.internal}")
   private String internalExchange;
 
   @Value("${rabbitmq.queues.notification}")
